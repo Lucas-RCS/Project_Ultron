587a009211a84ce27ce504e463b0f3638a779711
Lucas Ribeiro <lucasribeiro2216@gmail.com>
2023-10-18 20:55:43 -0300

    Created Uniform_Cost and Greedy codebase

--- Src/Resources/Greedy.py
+++ Src/Resources/Greedy.py
@@ -1,34 +1,32 @@
-from Src.Models.List import List
 from Src.Util.HexagonalCartesian import adjacent_coords, has_in_ambient
+from Src.Models.List import List
 
-class CustoUniforme(object):
-    
+class GreedySearch: # Busca gulosa (o_o)
+
+    # Construtor
     def __init__(self, ambient, beginning, destination):
         self.ambient = ambient
         self.beginning = beginning
         self.destination = destination
 
-    def search(self):
-
-        search_list = List() # Manipular a fila para a busca
-        copy_list = List() # Cópia para apresentar o caminho (somente inserção)
-
+    def greedy(self): 
+        # Manipular a fila para a busca
+        search_list = List()
+        copy_list = List()
+        visited = []
+            
         search_list.push(self.beginning, 0, None) # Insere ponto inicial como nó raiz da árvore
         copy_list.push(self.beginning, 0, None)
 
-        # Controle de nós visiteds
-        visited = []
-        line = [self.beginning, 0]
-        visited.append(line)
+        visited.append([self.beginning, 0])
 
-        # Verifica se é o objetivo
-        while not search_list.empty():
+        while not search_list.empty(): # Verifica se é o objetivo
             current = search_list.shift()
-            adjacents = adjacent_coords(current.coord) # Remove o primeiro da fila
-            
+            adjacents = adjacent_coords(current.coord)
+
             for coord in adjacents: # Varre todos as conexões dentro do grafo a partir de atual
                 if has_in_ambient(coord, self.ambient):
-                    flag = True # Pressupõe que não foi visitado
+                    flag = True
                     for j in range(len(visited)):
                         if visited[j][0] == coord:
                             if visited[j][1] <= (current.level + 1): # Se foi visitado, verifica se o nível é menor
@@ -40,11 +38,9 @@ class CustoUniforme(object):
                     if flag: # Se não foi visitado, inclui na fila
                         search_list.push(coord, current.level + 1, current)
                         copy_list.push(coord, current.level + 1, current)
+                        visited.append([coord, current.level + 1])
 
-                        line = [coord, current.level + 1]
-                        visited.append(line)
-
-                        if coord == self.destination:
+                        if coord == self.destination: # Se é o objetivo, retorna o caminho
                             path = copy_list.getPath()
                             return path
 
